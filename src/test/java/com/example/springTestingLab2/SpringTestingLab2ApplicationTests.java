f0eb491060ac8b5181b18b17867f797f422f167c
Sercan Tuna <ssercantuna@hotmail.com>
2022-08-09 08:45:16 -0400

    first commit

---
+++ src/test/java/com/example/springTestingLab2/SpringTestingLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springTestingLab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
